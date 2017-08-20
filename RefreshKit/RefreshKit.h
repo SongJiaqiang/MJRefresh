9d2890e3b3823ec82c423467505c45e32a0519c5
Jarvis <sjq2150@gmail.com>
2017-08-20 15:56:39 +0800

    Add shared framework target

---
+++ RefreshKit/RefreshKit.h
@@ -0,0 +1,21 @@
+//
+//  RefreshKit.h
+//  RefreshKit
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MJRefresh.h"
+
+//! Project version number for RefreshKit.
+FOUNDATION_EXPORT double RefreshKitVersionNumber;
+
+//! Project version string for RefreshKit.
+FOUNDATION_EXPORT const unsigned char RefreshKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RefreshKit/PublicHeader.h>
+
+
